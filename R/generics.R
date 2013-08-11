46235f713947e9a48bf180b021a6c7ce0d5084dd
tom buckley <buckley.tom@gmail.com>
2013-08-11 10:38:59 +0300

    stubbed out some classes and generic functions

--- R/generics.R
+++ R/generics.R
@@ -2,23 +2,33 @@
 #' 
 #' 
 TopoToSp <- function(x,arcs) {
-  UseMethod("TopoToSp", x)
+  UseMethod("TopoToSp", x, arcs)
 }
 
 TopoToSp.Arc <- function(x) {
   stop("Arcs do not contain object types like Polygon, MultiPolygon.  To translate individual arcs, try rel2abs.")
 }
 
-value.ArcIndex <- function(x) {
+TopoToSp.ArcIndex <- function(x) {
   stop("An ArcIndex do not contain object types like Polygon, MultiPolygon.  To translate individual arcs, try rel2abs.")
 }
 
-TopoToSp.Polygon <- function(x) {
-  # The class of the return vector needs to be "FibonacciData".
-  # We can do this by passing it to the constructor.
+rel2abs.Arc <- function(x,scale,translate) {
+	UseMethod("rel2abs", x, scale, translate)
+}
+
+rel2abs.ArcIndex <- function(x,scale,translate) {
+	UseMethod("rel2abs", x)
+}
+
+TopoToSp.Polygon <- function(x,scale,translate,arcs) {
   topo_poly_to_sp_poly(c(tail(x, 1), ifelse(!length(x), 1, sum(x))))
 }
 
-value.FibonacciData <- function(x) {
-  ifelse(length(x) == 0, 0, tail(x, 1))
+plot.TopoPolygon <- function(x) {
+
 }
+
+
+
+
