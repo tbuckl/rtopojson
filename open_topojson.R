9874c4a341548bbdb28db2a19ea7c5ba14dcff12
tom buckley <buckley.tom@gmail.com>
2013-08-12 23:39:09 +0300

    cleaned up and documented with RMarkdown

--- open_topojson.R
+++
@@ -1,7 +0,0 @@
-setMethod("open",
-    signature(con = "topojson"),
-    function (con, ...) 
-    {
-        stop("need a definition for the method here")
-    }
-)
--- testing.R
+++
@@ -1,8 +0,0 @@
-#testscript
-library(devtools)
-rm(list = ls())
-load_all()
-ca <- Topojson$new(file="inst/extdata/cali_nv_ariz.json")
-ca$open()
-ca$t2sp()
-ca$plot()
